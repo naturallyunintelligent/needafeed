7f193a46f89bc961bc6a931312d8279ebd38e82b
Darren Steele <darren.steele@five.ai>
2023-09-05 11:22:56 +0100

    initial main.py structure

---
+++ src/main.py
@@ -0,0 +1,35 @@
+def get_list_of_items():
+    # fetch list from notion(?) planner
+    pass
+
+
+def login_to_online_shop():
+    pass
+
+
+def empty_basket():
+    # empty basket if user confirms (otherwise add to existing)
+    pass
+
+
+def add_items_to_basket():
+    # auto add list of items to the basket, return a list of items not found
+    pass
+
+
+def manually_order_other_items():
+    # individually choose items not found and remember choices for next time
+    pass
+
+
+def confirm_and_complete():
+    pass
+
+
+def main():
+    get_list_of_items()
+    login_to_online_shop()
+    empty_basket()
+    add_items_to_basket()
+    manually_order_other_items()
+    confirm_and_complete()
