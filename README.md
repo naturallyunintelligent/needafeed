e8107b26d4351366227b9ce404d57b9163d57754
Darren Steele <darren.steele@five.ai>
2023-10-17 11:43:39 +0100

    get notion shopping list

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # needafeed
 WIP
+
 a meal planning / online shopping assistant project
+partly inspired by
+https://github.com/jaredkhan/sainsburys-assistant/blob/main/src/main.py
+
+
+Guide:
 - login to notion with hotmail
-- db id https://www.notion.so/Meal-Planner-fee5f10ec09247b784699a587b0df29d
+  - db id https://www.notion.so/Meal-Planner-fee5f10ec09247b784699a587b0df29d
+- Duplicate .env-sample and rename as .env
+- Paste the relevant values in place of the placeholders
+
 
 Useful links:
 - https://www.notion.so/089f64b5bbc54df2ad577b9092765e1f?v=c346b81ceefd4335a943e317b738c46b&pvs=4
 - https://developers.notion.com/
 - https://realpython.com/courses/inheritance-composition-python/
 - https://www.appsmith.com/use-cases
+- https://developers.notion.com/docs/create-a-notion-integration
--- src/main.py
+++ src/main.py
@@ -1,6 +1,40 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+class Settings:
+    notion_key: str = "<your-notion-api-key-should-be-set-in-.env>"
+    notion_list_page_id: str = "<shopping-list-page-id-should-be-set-in-.env>"
+
+    load_dotenv()
+
+
 def get_list_of_items():
-    # fetch list from notion(?) planner
-    pass
+    page_size = 1000
+    payload = {"page_size": page_size}
+    response = requests.post(
+        url=f"https://api.notion.com/v1/databases/"
+        f"{os.getenv('NOTION_LIST_PAGE_ID', 'notion_list_page_id')}/query",
+        json=payload,
+        headers={
+            "Authorization": f"Bearer {os.getenv('NOTION_KEY', 'notion_key')}",
+            "Notion-Version": "2022-06-28",
+            "Content-Type": "application/json",
+        },
+    )
+
+    notion_shoppinglist_json = json.loads(response.content.decode("utf-8"))
+
+    items = []
+    for item in notion_shoppinglist_json["results"]:
+        items.append(item["properties"]["Name"]["title"][0]["plain_text"])
+
+    # TODO: could add quantities / day / recipe links here?
+
+    return items
 
 
 def login_to_online_shop():
@@ -12,8 +46,12 @@ def empty_basket():
     pass
 
 
-def add_items_to_basket():
+def add_items_to_basket(list):
     # auto add list of items to the basket, return a list of items not found
+    # for item in list:
+    #     search_url =
+    #     f"https://www.tesco.com/groceries/en-GB/search?query={item}"
+
     pass
 
 
@@ -27,9 +65,14 @@ def confirm_and_complete():
 
 
 def main():
-    get_list_of_items()
+    Settings()
+    list = get_list_of_items()
     login_to_online_shop()
     empty_basket()
-    add_items_to_basket()
+    add_items_to_basket(list)
     manually_order_other_items()
     confirm_and_complete()
+
+
+if __name__ == "__main__":
+    main()
